3944ec08f21539b1735587bb4e2e9a3a05879c8e
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2025-02-25 15:17:02 +0530

    Completed JS-marathon day-2

---
+++ lecture/JS/JS-marathon/hoisting.js
@@ -0,0 +1,21 @@
+// console.log('Age is ', age);
+
+// TEMPORAL DEAD ZONE
+// const age = 25;
+
+// Reference Error: age is not defined
+// Cannot access 'age' before initialization
+
+test();
+
+function test() {
+    console.log("I am inside test function");
+}
+
+// age = 3000;
+console.log('Age is', age);
+
+var age = 24;
+
+console.log('Age is', age);
+
--- lecture/JS/JS-marathon/index.js
+++ lecture/JS/JS-marathon/index.js
@@ -24,8 +24,25 @@ const obj = {
 
 console.log('Hi');
 
-// setTimeout(obj.greet, 2 * 1000); // context of this fades in first call stack so after event //loop pushes from callback queue to call stack the function does not remember the context of this and hence we need to use bind in order to bind the context of this to the function call.
+// setTimeout(obj.greet, 2 * 1000); // context of this fades/gets deleted in first call stack so after event //loop pushes from callback queue to call stack the function does not remember the context of this and hence we need to use bind in order to bind the context of this to the function call.
 
-setTimeout(obj.greet.bind(obj), 2 * 1000);
+setTimeout(obj.greet.bind(obj), 0);
+
+Promise.resolve().then(() => {
+    console.log("1.Promise is resolved");
+
+    Promise.resolve().then(() => {
+        console.log("2.Promise is resolved");
+
+        Promise.resolve().then(() => {
+            console.log("3.Promise is resolved");
+
+            Promise.resolve().then(() => {
+                console.log("4.Promise is resolved");
+            });
+        });
+    });
+});
+
 
 console.log('Bye');
