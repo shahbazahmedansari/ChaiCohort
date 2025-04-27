fbcb1d097316233dc0d96d9899a1df8adabd9cb8
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2025-04-27 20:40:42 +0530

    Completed typescript overview

---
+++ lecture/TYPESCRIPT/src/app/routes/health/route.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import type { Router } from 'express';
+import HealthController from './controller';
+
+export function register(): Router {
+	const router = express.Router();
+	const controller = new HealthController();
+	router.get('/', controller.handleHealthCheck.bind(controller));
+	return router;
+}
