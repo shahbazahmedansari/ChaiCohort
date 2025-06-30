221ebab722e6296f333e743b6f0405c11044c717
Shahbaz-1696 <ansarishahbaz96@gmail.com>
2025-06-30 21:19:44 +0530

    Added register controller for TaskManager

--- projects/TaskManager/src/controllers/auth.controllers.js
+++ projects/TaskManager/src/controllers/auth.controllers.js
@@ -1,10 +1,74 @@
 import { asyncHandler } from "../utils/async-handler.js";
+import { User } from "../models/user.models.js";
+import bcrypt from "bcryptjs";
 
 const registerUser = asyncHandler(async (req, res) => {
     const { email, username, password, role } = req.body;
 
     // validation
+    try {
+        const existingUser = await User.findOne({
+            where: {
+                email,
+            }
+        });
+
+        if (existingUser) {
+            return res.status(400).json({
+                message: "User already exists",
+                success: false,
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const newUser = await User.create({
+            email,
+            password: hashedPassword,
+            username,
+            role,
+        });
+
+        if (!newUser) {
+            return res.status(400).json({
+                message: "User not created",
+                success: false,
+            });
+        }
+
+        return res.status(201).json({
+            message: "User created successfully",
+            success: true,
+            user: {
+                email,
+                username,
+                role,
+            }
+        });
+    } catch (error) {
+        console.log("Error in creating the user", error);
+        return res.status(500).json({
+            message: "Error in creating the user",
+            success: false,
+            error,
+        });
+    }
 
 });
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+
+    } catch (error) {
+        console.log("Error in logging in the user", error);
+        return res.status(500).json({
+            message: "Error in logging in the user",
+            status: false,
+            error,
+        });
+    }
+});
+
+export { registerUser, loginUser };
--- projects/TaskManager/src/routes/auth.routes.js
+++ projects/TaskManager/src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import { validate } from "../middlewares/validator.middleware.js";
-import { userRegistrationValidator } from "../validators/index.js";
-import { registerUser } from "../controllers/auth.controllers";
+import { userRegistrationValidator, userLoginValidator } from "../validators/index.js";
+import { registerUser, loginUser } from "../controllers/auth.controllers";
 
 const router = Router();
 
 // Factory Pattern: forcfully executing the userRegistrationValidator() which returns an array which is then passed to validate function to check for the errors
 router.route("/register").post(userRegistrationValidator(), validate, registerUser);
+
+router.route("/login").post(userLoginValidator(), validate, loginUser);
 
 export default router;
